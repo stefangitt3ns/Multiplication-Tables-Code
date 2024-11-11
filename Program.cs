43b2767e6f495ccd99a36a87a374bb2a479072a9
Stefan <Stefan@DESKTOP-P1OADI2>
2024-11-10 22:47:09 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiplication_Tables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         Console.Title = "Multiplication Tables";
+
+            Console.WriteLine("What times table do you need?");
+            int result = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= 12; i++)
+            {
+                int number = result * i;
+                Console.WriteLine(number);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
